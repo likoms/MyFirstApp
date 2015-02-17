7b01d49c6f43baaaf778cab5299ba6e081c918a3
likoms <toribosh@gmail.com>
2015-02-17 17:21:55 +0100

    Button does not work propertly.

--- app/src/main/java/com/likoms/myfirstapp/MainActivity.java
+++ app/src/main/java/com/likoms/myfirstapp/MainActivity.java
@@ -4,36 +4,33 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static com.likoms.myfirstapp.R.color.red;
 
 
 public class MainActivity extends ActionBarActivity {
+    private TextView textView = (TextView) findViewById(R.id.textView);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        changeTextView("Hello my friend");
+
+    }
+
+    private void changeTextView(String s) {
+       textView.setText(s);
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void doSomething(View view) {
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String input = editText.getText().toString();
+        textView.setText("Hello" + input);
+        textView.setTextColor(getResources().getColor(R.color.red));
     }
 }
