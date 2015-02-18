cdb4a5654b1ce674e2cb528f9eb08cd4b04f696c
likoms <toribosh@gmail.com>
2015-02-18 19:11:35 +0100

    rebulid app.

--- app/src/main/java/com/likoms/myfirstapp/MainActivity.java
+++ app/src/main/java/com/likoms/myfirstapp/MainActivity.java
@@ -5,11 +5,15 @@ import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.content.Intent;
+
+import static com.likoms.myfirstapp.R.layout.activity_display_message;
 
 
 public class MainActivity extends ActionBarActivity  {
@@ -21,12 +25,19 @@ public class MainActivity extends ActionBarActivity  {
         setContentView(R.layout.activity_main);
     }
 
-    public void sendMessage(View view) {
+
+
+    /*public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.edit_message);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
-    }
+    }*/
 
-}
+    public void openDisplayMsgActivity(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        startActivity(intent);
+
+        }
+    }
