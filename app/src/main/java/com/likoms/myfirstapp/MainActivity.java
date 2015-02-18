ebdec2d1ca3ae44222a20a144f5566a96bf5c60e
likoms <toribosh@gmail.com>
2015-02-18 09:29:17 +0100

    Add second activity. Fix the button

--- app/src/main/java/com/likoms/myfirstapp/MainActivity.java
+++ app/src/main/java/com/likoms/myfirstapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.likoms.myfirstapp;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -7,30 +9,24 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.content.Intent;
 
-import static com.likoms.myfirstapp.R.color.red;
 
-
-public class MainActivity extends ActionBarActivity {
-    private TextView textView = (TextView) findViewById(R.id.textView);
+public class MainActivity extends ActionBarActivity  {
+    public final static String EXTRA_MESSAGE = "com.likoms.myfirstapp.MESSAGE";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        changeTextView("Hello my friend");
-
     }
 
-    private void changeTextView(String s) {
-       textView.setText(s);
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 
-
-    public void doSomething(View view) {
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String input = editText.getText().toString();
-        textView.setText("Hello" + input);
-        textView.setTextColor(getResources().getColor(R.color.red));
-    }
 }
